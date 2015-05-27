ae88b1a102d42e99a0d30ea3f45c956c9fa5e756
root <root@debian-master>
2015-05-27 04:24:47 +0000

    a little work

---
+++ chooseops.c
@@ -0,0 +1,12 @@
+//Program meant to test techniques for acting on which operator the user chooses.
+
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+int ops[3]={'a','b','c'};
+if (argc != 2){
+	printf("usage: [a] or [b] or [c]");
+}
+
+}
