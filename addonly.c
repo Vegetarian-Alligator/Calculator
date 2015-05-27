aecc45693f9ad6c69ed06fc2f5a9814b8980271b
root <root@debian-master>
2015-05-27 06:03:14 +0000

    Discovered a radically more efficient way to do this.  I think.

--- addonly.c
+++ addonly.c
@@ -2,15 +2,9 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char *argv[]){
-char test[99]={"abcdefghijklmnopqrstuvwxyz"};
-
-printf("Hardcoded test: %i\n",(test-&test[15])/sizeof(char));
-return 0;
-
-int ops[3]={'a','b','c'};
-char buffer;
 int i,i2;
 
 char *plussign;
@@ -18,21 +12,32 @@ char *lastplus;
 int index;
 index=-1;
 
+int base=10;
+
 //I have a better idea...
+
+long result=0;
 
 for (i=1;i!=argc;i++)
 {
-
-
 	i2=0;
-	while (argv[i][i2++]!='+') plussign=&argv[i][i2];
-	index++;
-	if (index){	
-		index=-1;
-		printf("Space between the plus signs: %i charactersi\n",(plussign - lastplus)/sizeof(char) );
-	}else {lastplus=plussign;}
-
+	while (i2!=strlen(argv[i])){
+		while (argv[i][i2++]!='+') plussign=&argv[i][i2];
+		printf("INdex: %i\n",index);
+		index++;
+		if (index){	
+			index=-1;
+		}else {lastplus=plussign;}
+	}
 	
+	if (lastplus==plussign && index !=-1){ //Still assumes an equal sign is present.
+		lastplus=argv[i];
+		int subgoal=(plussign-lastplus)/sizeof(char)-1; //The actual position is included in this count, not just the space between!
+		int subcount;
+		int (subcount=0;subcount<subgoal;subcount++){
+			result += int(argv[subcount] - '0')*pow(subcount,base);	
+		}
+	}	
 }
 
 printf("\n\n");	
