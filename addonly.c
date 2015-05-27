980196fd0706000f8299862a260782e00a589a76
root <root@debian-master>
2015-05-27 06:42:11 +0000

    Segmentation fault and I don't know why.

--- addonly.c
+++ addonly.c
@@ -5,48 +5,48 @@
 #include <math.h>
 
 int main(int argc, char *argv[]){
-int i,i2;
 
-char *plussign;
-char *lastplus;
-int index;
-index=-1;
-
-int base=10;
-
-//I have a better idea...
-
-long result=0;
-
-for (i=1;i!=argc;i++)
-{
-	i2=0;
-	while (i2!=strlen(argv[i])){
-		while (argv[i][i2++]!='+') plussign=&argv[i][i2];
-		printf("INdex: %i\n",index);
-		index++;
-		if (index){	
-			index=-1;
-		}else {lastplus=plussign;}
-	}
-	
-	if (lastplus==plussign && index !=-1){ //Still assumes an equal sign is present.
-		lastplus=argv[i];
-		int subgoal=(plussign-lastplus)/sizeof(char)-1; //The actual position is included in this count, not just the space between!
-		int subcount;
-		int (subcount=0;subcount<subgoal;subcount++){
-			result += int(argv[subcount] - '0')*pow(subcount,base);	
-		}
-	}	
-}
-
-printf("\n\n");	
 
 if (argc != 2){
-	printf("usage: [a] or [b] or [c]");
+	printf("number + number");
 	return 2;
+}
+
+int base=10; //later to be specified by the user
+
+int result=0; //should be long
+int subresult=0; //should be long
+int argindex=1;
+int i=0;
+int powindex=0;
+int convertchar;
+
+printf("test");
+/*
+while (argindex++!=argc){
+	subresult=0;
+	while (i!=strlen(argv[argindex])){
+		convertchar= argv[argindex][i]-'0';
+		exit(0);
+		printf("Yes or no.");
+		if (convertchar < 0 || convertchar > 9){
+			if (argv[argindex][i]=='+'){
+				powindex=0;
+				result+=subresult;
+				subresult=0;
+				continue;
+			}else{
+				printf("Error!  Invalid Operation Specified\n");
+				return 2;
+			}
+		}
+		subresult+=convertchar*pow(base,powindex++);
+	printf("added something\n");	
 }
 
 //printf("Your chosen opnumber was:  %i",i);
 
+}
+//printf("The result of your additions was: %i",result);
+*/
 }
